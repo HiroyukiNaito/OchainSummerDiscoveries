03bc9f4e1cdeff262e6895f7d03f63cf38cc7f79
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2024-07-07 13:38:28 +0900

    Show favoriete in appCards

--- src/lib/threeFunc.ts
+++ src/lib/threeFunc.ts
@@ -2,7 +2,13 @@ import { ImageCacheData, SvgCacheData } from "@/types/api";
 import * as THREE from 'three';
 import SpriteText from 'three-spritetext';
 
-const HEART_SVG = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 107.39"><defs><style>.cls-1{fill:#ed1b24;fill-rule:evenodd;}</style></defs><title>red-heart</title><path class="cls-1" d="M60.83,17.18c8-8.35,13.62-15.57,26-17C110-2.46,131.27,21.26,119.57,44.61c-3.33,6.65-10.11,14.56-17.61,22.32-8.23,8.52-17.34,16.87-23.72,23.2l-17.4,17.26L46.46,93.55C29.16,76.89,1,55.92,0,29.94-.63,11.74,13.73.08,30.25.29c14.76.2,21,7.54,30.58,16.89Z"/></svg>';
+const HEART_SVG = `<svg id="favorite-heart" data-name="favorite-heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 107.39">
+                       <defs>
+                         <style>.cls-1{fill:#ed1b24;fill-rule:evenodd;}</style>
+                       </defs>
+                       <title>favorite-heart</title>
+                       <path class="cls-1" d="M60.83,17.18c8-8.35,13.62-15.57,26-17C110-2.46,131.27,21.26,119.57,44.61c-3.33,6.65-10.11,14.56-17.61,22.32-8.23,8.52-17.34,16.87-23.72,23.2l-17.4,17.26L46.46,93.55C29.16,76.89,1,55.92,0,29.94-.63,11.74,13.73.08,30.25.29c14.76.2,21,7.54,30.58,16.89Z"/>
+                    </svg>`;
 const blob = new Blob([HEART_SVG], { type: 'image/svg+xml' });
 const heart_blob = URL.createObjectURL(blob)
 
@@ -172,6 +178,7 @@ export const appCard = (node: any, currentCache: ImageCacheData[]) => `
   <div>
     <div>
         <div className="flex flex-row justify-between">
+          ${localStorage.getItem(node.id)?favoriteIcon():""}
           <div style="text-align: center; display: block;">
             <Image
               src="${deriveBase64DataFromCache(node, currentCache)}"
@@ -191,4 +198,12 @@ export const appCard = (node: any, currentCache: ImageCacheData[]) => `
   </div>
 </div>
 `;
+
+const favoriteIcon = () =>`
+<Image
+  src="${heart_blob}"
+  style="border-radius: 3px; background-color: rgba(255, 255, 255, 0); display: block; margin-right: auto; width: 5%;"
+  alt="favorite-heart"
+/>
+`
 
