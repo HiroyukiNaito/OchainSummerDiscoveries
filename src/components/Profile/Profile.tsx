4655c72e2f0402f60c83e01343da1cf35dc2a159
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2024-07-25 20:22:10 +0900

    Changed Profile design and added toggle button

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,7 +1,4 @@
-// components/Avatar.tsx
-'use client';
-
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import { useAccount, useBalance, useEnsAvatar, useEnsName } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
 import { formatEther } from 'viem';
@@ -13,6 +10,7 @@ type Props = {
 };
 
 const Profile = ({ children }: Props) => {
+  const [isHidden, setIsHidden] = useState(false);
   const { address } = useAccount();
   const { data: balanceData } = useBalance({ address });
   const { data: ensName } = useEnsName({ address, chainId: mainnet.id });
@@ -25,26 +23,38 @@ const Profile = ({ children }: Props) => {
 
   const formattedBalance = balanceData ? `${formatBalance(balanceData.value, 4)} ${balanceData.symbol}` : '-';
 
+  const toggleProfile = () => {
+    setIsHidden(!isHidden);
+  };
+
   return (
-    <div className={styles.profileContainer}>
-      <div className={styles.header}>
-        <AvatarView imgSrc={avatar ?? '/baselogo.png'} />
-        <div className={styles.name}>{ensName ? ensName
-          : address ? address
-            : 'Not connected!'}</div>
-      </div>
-      {children}
-      <div className={styles.infoGrid}>
-        <div className={styles.infoItem}>
-          <div className={styles.infoLabel}>Address</div>
-          <div className={`${styles.infoValue} ${styles.addressValue}`}>{address ?? '-'}</div>
+    <>
+       <button 
+        className={`${styles.toggleButton} ${isHidden ? styles.extended : ''}`} 
+        onClick={toggleProfile}
+      >
+        {isHidden ? 'Extend': 'Hide'}
+      </button>
+      <div className={`${styles.profileContainer} ${isHidden ? styles.hidden : ''}`}>
+        <div className={styles.header}>
+          <AvatarView imgSrc={avatar ?? '/baselogo.png'} />
+          <div className={styles.name}>{ensName ? ensName
+            : address ? address
+              : 'Not connected!'}</div>
         </div>
-        <div className={styles.infoItem}>
-          <div className={styles.infoLabel}>Balance</div>
-          <div className={styles.infoValue}>{formattedBalance}</div>
+        {children}
+        <div className={styles.infoGrid}>
+          <div className={styles.infoItem}>
+            <div className={styles.infoLabel}>Address</div>
+            <div className={`${styles.infoValue} ${styles.addressValue}`}>{address ?? '-'}</div>
+          </div>
+          <div className={styles.infoItem}>
+            <div className={styles.infoLabel}>Balance</div>
+            <div className={styles.infoValue}>{formattedBalance}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
