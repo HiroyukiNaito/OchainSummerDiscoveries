f309245836f800a1ceeb02e371915ef3fd87ca3a
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2024-08-02 22:31:33 +0900

    Refactored api/download

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -1,21 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
-
-export async function POST(request: NextRequest) {
-  const pinataApiKey = process.env.PINATA_API_KEY;
-  const pinataSecretApiKey = process.env.PINATA_SECRET_API_KEY;
-
-  if (!pinataApiKey || !pinataSecretApiKey) {
-    return NextResponse.json({ error: 'Pinata API keys are not set in the environment variables' }, { status: 500 });
-  }
-  const body = await request.json();
-  const fileName = body.data.name;
-  if (!fileName) {
-    return NextResponse.json({ error: 'Invalid file name' }, { status: 400 });
-  }
-
+export const POST = async (request: NextRequest) => {
   try {
+    const pinataApiKey = process.env.PINATA_API_KEY;
+    const pinataSecretApiKey = process.env.PINATA_SECRET_API_KEY;
+
+    if (!pinataApiKey || !pinataSecretApiKey) {
+      throw new Error('Pinata API keys are not set in the environment variables!');
+    }
+
+    const body = await request.json();
+    const fileName = body.data?.name;
+    if (!fileName) {
+      return NextResponse.json({ error: 'Invalid file name' }, { status: 400 });
+    }
+
     // Dynamically import Pinata SDK
     const pinataSDK = (await import('@pinata/sdk')).default;
     const pinata = new pinataSDK({ pinataApiKey, pinataSecretApiKey });
@@ -30,19 +29,21 @@ export async function POST(request: NextRequest) {
     });
 
     // If file is found
-    if (response && response.rows && response.rows.length > 0) {
-      const file = response.rows[0];
-      const fileHash = file.ipfs_pin_hash;
+    if (response?.rows?.length > 0) {
 
+      const fileHash = response.rows[0].ipfs_pin_hash;
       // Construct the download URL
       const downloadUrl = `https://gateway.pinata.cloud/ipfs/${fileHash}`;
 
       return NextResponse.json({ downloadUrl });
     } else {
+
       return NextResponse.json({ error: 'File not found' }, { status: 404 });
     }
   } catch (error) {
-    console.error(error);
+
+    console.error('Error fetching file from Pinata:', error);
+
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
