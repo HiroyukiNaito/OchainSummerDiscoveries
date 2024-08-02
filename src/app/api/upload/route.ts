e43875b70153df46886e21218469c09f863b1cbf
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2024-08-02 22:03:46 +0900

    Refactor upload api code

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,17 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
-import pinataSDK from '@pinata/sdk';
+
 export const dynamic = 'force-dynamic';
-export async function POST(request: NextRequest) {
+
+export const POST = async (request: NextRequest) => {
   try {
     const pinataApiKey = process.env.PINATA_API_KEY;
     const pinataSecretApiKey = process.env.PINATA_SECRET_API_KEY;
 
     if (!pinataApiKey || !pinataSecretApiKey) {
-      throw new Error('Pinata API keys are not set in the environment variables');
+      throw new Error('Pinata API keys are not set in the environment variables!');
     }
 
+    // Dynamically import Pinata SDK
+    const pinataSDK = (await import('@pinata/sdk')).default;
     const pinata = new pinataSDK({ pinataApiKey, pinataSecretApiKey });
-    ;
 
     const body = await request.json();
     const data = body.data;
@@ -28,7 +30,7 @@ export async function POST(request: NextRequest) {
 
     const result = await pinata.pinJSONToIPFS(data, options);
 
-    // Fetch the file from Pinata
+    // Fetch the file from Pinata by name
     const response = await pinata.pinList({
       status: 'pinned',
       metadata: {
@@ -37,18 +39,19 @@ export async function POST(request: NextRequest) {
       }
     });
 
-    // If file is found
+    // If duplicate files are found, unpin the older ones
     if (response && response.rows && response.rows.length > 1) {
       const file = response.rows[1];
       console.log(file);
       const fileHash = file.ipfs_pin_hash;
-      pinata.unpin(fileHash);
+      await pinata.unpin(fileHash);
     }
-
 
     return NextResponse.json({ ipfsHash: result.IpfsHash });
   } catch (error) {
+
     console.error('Error uploading data to IPFS:', error);
+
     return NextResponse.json({ error: 'Error uploading data to IPFS' }, { status: 500 });
   }
 }
